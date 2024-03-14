63248c6539c5b309a8517c5e5c745f7226842adc
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-14 14:04:54 +0300

    cats

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats =[ "Milo" ,"Otis" , "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    const newCats =[...cats,name]
+    return newCats
+}
+function prependCat(name){
+    const prepCat =[name,...cats]
+    return prepCat
+}
+function removeLastCat(name){
+    const rmvCat =cats.slice(0,-1)
+    return rmvCat
+}
+function removeFirstCat(name){
+    const rmvFirstCat=cats.slice(1)
+    return rmvFirstCat
+}
